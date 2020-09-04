cf3c57c7fff9bdb92907843711d877d7dffb4de3
elano <elanor.melo@gmail.com>
2020-09-03 22:39:57 -0300

    proteger chaves:

--- backend/src/storage-engine.js
+++ backend/src/storage-engine.js
@@ -2,12 +2,10 @@ const multer = require('multer')
 const MulterAzureStorage = require('multer-azure-blob-storage').MulterAzureStorage;
 
 const azureStorage = new MulterAzureStorage({
-    connectionString: 'DefaultEndpointsProtocol=https;AccountName=vmdad2020groupdiag;AccountKey=BjCMKLtg7ABjYHZ/hDC2UK+NUtBK/GXaz1znl8UTNWBXIqHoQfB8X1ZE4COFdaEGPT/jvAEIsiBeUlVZKG3uWQ==;EndpointSuffix=core.windows.net',
-    accessKey: 'BjCMKLtg7ABjYHZ/hDC2UK+NUtBK/GXaz1znl8UTNWBXIqHoQfB8X1ZE4COFdaEGPT/jvAEIsiBeUlVZKG3uWQ==',
+    connectionString: '',
+    accessKey: '',
     accountName: 'vmdad2020groupdiag',
     containerName: 'documentos',
-    // blobName: resolveBlobName,
-    // metadata: resolveMetadata,
     containerAccessLevel: 'blob',
     urlExpirationTime: 60
  });
